d7166278b315f21dd6ffc742cf97624b50616157
FastestMenAlive <erick_pimo98@hotmail.com>
2019-08-02 20:32:31 -0600

    contacto-proovedores
    modelo, controlador y tabla de contacto-proveedor

--- resources/views/tablaProveedor.blade.php
+++ resources/views/tablaProveedor.blade.php
@@ -62,6 +62,65 @@
         @endforeach()
 		</table>
 	</div>
+
+	<div class="card">
+		<div class="row mrg2">
+			<div class="input-field col m9">
+	            <i class="material-icons prefix">search</i>
+	            <input id="busMContacto" type="text" class="validate">
+	            <label for="busContacto">Contactos</label>
+            </div>
+            <div class="input-field col m3">
+            <button class="waves-effect waves-light btn modal-trigger" data-target="agContacto">Agregar Contacto</button>
+            <!-- Modal Structure -->
+            <form name="FrmContactos">
+                <div id="agContacto" class="modal">
+                    <div class="modal-content">
+                        <h4 class="black-text center">CONTACTO CON PROVEEDORES</h4>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="nomEmpresa" type="text" class="validate" ng-model="contactos.nombre">
+                                <label for="nomEmpresa">Nombre deL contacto</label>
+                            </div>
+                            <div class="input-field col s12">
+                                <input id="dirEmpresa" type="text" class="validate" ng-model="contactos.telefono">
+                                <label for="dirEmpresa">Teléfono del contacto</label>
+                            </div>
+                            <div class="input-field col s12">
+                                <input id="telefono" type="number" class="validate" ng-model="contactos.correo">
+                                <label for="telefono">Correo del contacto</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#!" name="btnGuardar" class="modal-close waves-effect waves-green btn-flat" ng-click="GuardarCon()">Agregar</a>
+                    </div>
+                </div>
+            </form>
+    	</div>
+		</div>
+	</div>
+
+	<div class="card">
+		<table>
+		<tr>
+			<th>Nombre</th>
+			<th>Telefono</th>
+			<th>Correo</th>
+			<th>Editar</th>
+			<th>Deshabilitar</th>
+		</tr>
+		@foreach ($proveedores as $proveedor)
+           <tr ng-click="seleccionar($index)">
+                <td><?= $proveedor->empresa ?></td>
+                <td><?= $proveedor->direccion ?></td>
+                <td><?= $proveedor->telefono ?></td>
+                <td><button class="waves-effect amber accent-3 btn modal-trigger"  href="#modalEditarMaterial"><i class="material-icons">edit</i></button></td>
+            	<td><div class="switch">    <label>            <input type="checkbox">      <span class="lever"></span>          </label>  </div></td>
+           </tr>
+        @endforeach()
+		</table>
+	</div>
 	</div>
 	
 
@@ -79,6 +138,8 @@
     	app.controller('ctrl', function($scope, $http){
             $scope.proveedores = {};
 
+            $scope.contactos = {};
+
                 $scope.GuardarPro=function(){
                 
                 $http.post('/GuardarPro',$scope.proveedores).then(
@@ -95,6 +156,19 @@
             $scope.modificar = function(index){
                 $scope.provedoores = $scope.proveedores[index];
             }
+
+			$scope.GuardarCon=function(){
+                
+                $http.post('/GuardarCon',$scope.contactos).then(
+                    function(response){
+                        alert('GUARDADO');
+                    },
+                    function(erroResponse){
+                        console.log(erroResponse);
+                    }
+                    );
+                	location.reload();
+                }            
 
 
         });
